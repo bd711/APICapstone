7e3637407d7222bb8c1746f556046e1b6f1ed853
bd711 <bridget.dunn3@gmail.com>
2016-11-18 20:13:51 -0500

    Add files via upload

---
+++ BDCapstone/README.md
@@ -0,0 +1,32 @@
+# APICapstone
+
+## Summary
+### "Let's Walk to Lunch" allows users to find nearby lunch options based on current weather conditions. Users enter their workplace address. Current weather conditions are retrieved from WeatherUnderground, and are used to determine whether lunch today should be a longer walk (warm, sunny conditions) or a quick dash from the office (cold, rainy, or snowy conditions). Restaurants within an appropriate radius must be open for lunch, rated 3 stars or higher, and priced at $$ or less. All results are returned with their address, link to Yelp page, and plotted on a navigable map. 
+
+##Let's Walk to Lunch: 
+###User enters their address in the website 
+![alt text][search]
+
+[search]: https://github.com/bd711/APICapstone/blob/master/screenshot1.png "Search Criteria"
+
+###Eligible restaurants are returned in a list based on the current weather conditions 
+
+![alt text][return]
+
+[return]: https://github.com/bd711/APICapstone/blob/master/screenshot2.png "List of elgible restaurants"
+
+###Eligible restaurants are mapped 
+
+![alt text][map]
+
+[map]: https://github.com/bd711/APICapstone/blob/master/screenshot3.png "Map"
+
+
+
+## Technology Used 
+* HTML
+* CSS 
+* JavaScript 
+* WeatherUnderground API
+* Yelp API
+* Google Maps API 
---
+++ BDCapstone/app.js
@@ -0,0 +1,109 @@
+var map;
+$(document).ready(function() { // get location from user
+    $('form').submit(function(event) {
+        event.preventDefault();
+        var street = $(".js_street").val();
+        var state = $(".js_state").val();
+        var city = $(".js_city").val();
+        var userInput = state + '/' + city; // format location info to be used in weather underground
+        var totalAddress = '"' + street + ' ' + city + ',' + state + '"';
+        console.log(userInput); // location info is displayed correctly 
+        $(".results").empty(); // clear out any old results 
+
+
+
+
+        $.ajax({ // feed location into API, get back conditions 
+            url: 'https://api.wunderground.com/api/af91e5347e902bf9/conditions/settings/q/' + userInput + '.json',
+            dataType: "json",
+            method: "GET",
+            success: function(data) {
+                console.log(data);
+
+
+                console.log(data.current_observation.temp_f);
+                var temperature = data.current_observation.temp_f;
+                var weather = data.current_observation.weather;
+                var distance = 1000;
+                if (temperature < 50 || weather == "raining" || weather == "snowing") {
+                    distance = 250;
+                }
+
+                $.ajax({
+                    url: 'https://arcane-ridge-20214.herokuapp.com/api/search/' + distance + '/' + totalAddress,
+                    dataType: "json",
+                    method: "GET",
+                    success: function(yelpData) {
+                        console.log(yelpData);
+
+
+                        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                        var labelIndex = 0;
+                        var labels2 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                        var labelIndex2 = 0
+
+                        $.each(yelpData.businesses, function(index, business) {
+                            $(".results").append("<li><ol>" + labels[labelIndex++ % labels.length] + ". " + business.name + " &#9679; " + business.location.address + " <a href=" + business.url + " target='_blank'>&#9679; Learn More</a>" + "</ol></li>");
+
+                            var locations = { lat: business.location.coordinate.latitude, lng: business.location.coordinate.longitude }; // make this an array of the different locations returned
+                            var marker = new google.maps.Marker({
+                                position: locations,
+                                map: map,
+                                label: labels[labelIndex2++ % labels2.length],
+                            });
+
+                            map.setCenter({ lat: business.location.coordinate.latitude, lng: business.location.coordinate.longitude });
+
+
+                        });
+                    }
+                });
+
+
+
+            }
+
+
+
+
+        });
+    });
+});
+
+
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 42.3601, lng: -71.0589 },
+        zoom: 14
+    });
+
+}
+
+// get data back from weather underground 
+// if temp_f <45 or raining or snowing, send badWeather search to yelp 
+// else, send goodWeather search to Yelp
+// how to do two separate calls to yelp? Put right in the if/else statement? 
+
+
+
+// display in list + show on map as pins 
+
+
+
+
+
+/* Yelp info: Consumer Key  o5WlNJkfPxgXNgC9VUvrAQ
+Consumer Secret quI2oBuTlsZdzX544UVhL4YaD6s
+Token   KU6chDhw8OWOxkA4hXKDSoJYAav7WWdG
+Token Secret    FHXrR-WdLCTXpiKs4vcwhTAduAg
+
+http://api.wunderground.com/api/af91e5347e902bf9/conditions/q/MA/Boston.json
+
+from weather underground, get back the following fields from Conditions: 
+temp_f
+weather
+
+weather underground key: "af91e5347e902bf9"
+Project Name: "Let\'s Walk to Lunch"
+*/
